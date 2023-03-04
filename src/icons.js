6f451e414b2cb4b87ec532d5f2707f2291daad37
peterliujp0 <35127132+peterliujp0@users.noreply.github.com>
2023-03-04 11:42:54 -0800

    modification 03/23

--- src/icons.js
+++ src/icons.js
@@ -24,7 +24,7 @@ export const ChevronDown = () => {
       className='h-6 w-6'
       fill='none'
       viewBox='0 0 24 24'
-      stroke='currentColor'
+      stroke='currentColor' 
       strokeWidth={2}
     >
       <path strokeLinecap='round' strokeLinejoin='round' d='M19 9l-7 7-7-7' />
