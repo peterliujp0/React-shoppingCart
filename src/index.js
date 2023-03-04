e08f7cc5b2b8d7667f1fe9351e5cbedef2255232
peterliujp0 <35127132+peterliujp0@users.noreply.github.com>
2023-03-04 11:24:04 -0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import { Store } from '@reduxjs/toolkit';
+import { Provider } from "react-redux";
+import { store } from "./store";
+import ReactDOM from "react-dom";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
